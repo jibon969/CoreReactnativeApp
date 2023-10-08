10d4ad47ee5078c60144e79b6afd249d278ad934
jibon969 <jayed.swe@gmail.com>
2023-10-08 22:43:33 +0600

    added some file

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import {SafeAreaView, StyleSheet, Text, View, StatusBar} from 'react-native';
 import React from 'react';
-import LoginScreen from './src/screens/LoginScreen';
+import {NavigationContainer} from '@react-navigation/native'
+import DrawerNavigator from './src/navigation/DrawerNavigator'
 
 const App = () => {
     return (
         <>
             <SafeAreaView style={styles.safeArea}>
                 <StatusBar backgroundColor="#fff" barStyle="dark-content"/>
-                <LoginScreen/>
+                <NavigationContainer>
+                    <DrawerNavigator/>
+                </NavigationContainer>
             </SafeAreaView>
             <View style={styles.statusBarFooter}/>
         </>
